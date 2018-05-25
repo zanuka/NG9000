b33f70b68cfddadfe4fe1d420a09f9009acf8b7f
zanuka <202134+zanuka@users.noreply.github.com>
2018-05-26 00:51:47 +1200

    wire up polyfills for custom-elements

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -78,3 +78,8 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+import '@webcomponents/custom-elements/src/native-shim';
+import '@webcomponents/custom-elements/custom-elements.min'
+
+
