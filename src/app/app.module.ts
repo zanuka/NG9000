40d2848f3c8f2fa46eab25f284a5b1740f627978
zanuka <202134+zanuka@users.noreply.github.com>
2018-05-26 01:26:17 +1200

    remove my-nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MyNavComponent } from './my-nav/my-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import { ClarityModule } from '@clr/angular';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyNavComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
