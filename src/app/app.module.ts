7dab3a6c53c71304043a9c4d488442231e387989
zanuka <git@zanuka.com>
2018-06-03 23:29:59 +1200

    adds two-way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -19,6 +20,7 @@ import { ClarityModule } from '@clr/angular';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     NgbModule.forRoot(),
     BrowserAnimationsModule,
     LayoutModule,
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,16 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  // template: `
-  //   <app-server></app-server>
-  //   <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
-  serverName = '';
+
+  // now two-way bound via ngModel
+  serverName = 'initital server name';
 
   constructor() {
     setTimeout(() => {
