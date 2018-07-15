a3c43c88cf4bdd9ec9551daf7c756477f6274a00
Mike Delucchi <git@zanuka.com>
2018-07-15 23:14:06 +1200

    adds ngStyle :: getColor for online/offline servers

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
+import {min} from 'rxjs/operators';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
-  serverId: number = 10;
+  // generate some random server numbers
+  min = Math.ceil(100);
+  max = Math.floor(500);
+  serverId: number = Math.floor(Math.random() * (this.max - this.min)) + this.min;
   serverStatus: string = 'offline';
+
+  constructor() {
+    // randomly set server status for now
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline'
+  }
 
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+
   }
 }
